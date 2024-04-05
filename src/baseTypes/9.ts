44fc927a9e4d06a2eae15b4a50ae203fd7ec1ecf
talamus1972 <talamus1972@gmail.com>
2024-04-05 20:54:28 +0300

    1

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,6 +1,16 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+type Page = {
+    title: string;
+    likes: number;
+    accounts: string[];
+    status: 'open' | 'close';
+    details?: {
+        createAt: Date;
+        updateAt: Date;
+    };
+};
 
 const page1 = {
   title: 'The awesome page',
