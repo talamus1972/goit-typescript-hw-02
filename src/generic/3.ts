f577c8695f2b44202d44cdd6d214eafd98cc8754
talamus1972 <talamus1972@gmail.com>
2024-04-06 04:51:26 +0300

    6

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,11 +3,9 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge<T, U>(objA: T, objB: U): T & U{} {
-  return Object.assign(objA, objB);
+function merge<T, U>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
-
-const merged = merge({ name: 'Alisa' }, { age: 28 });
 
 
 export {};
